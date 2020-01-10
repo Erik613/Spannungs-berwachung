47195b6ef40a2b2b19fc78ca95ecbf271e5c5d7f
Patrick <patrickmatern1997@gmail.com>
2020-01-10 11:09:58 +0100

    Grund für Serial (Python / Arduino)

--- Py2Ardu.py
+++ Py2Ardu.py
@@ -1,9 +1,24 @@
 import serial
+import traceback
+import datetime
+
+a = 0
 
 while True:
-    with serial.Serial('COM3', 9600, ) as ser:
-        x = ser.read()
+    with open("log.txt", "a") as log:
         try:
-            print(int(x))
+            with serial.Serial('COM3', 9600, ) as ser:
+                x = ser.read()
+                b = datetime.datetime.now().strftime("%d.%m.%Y - %H:%M:%S") , "|" , int(x)
+                print(b)
+                log.write(str(b)+"\n")
+
+                a = 0
+        except serial.serialutil.SerialException:
+            a += 1
+            if a == 1:
+                print(datetime.datetime.now().strftime("%d.%m.%Y - %H:%M:%S"), "| Hardware wurde Entfernd")
+                traceback.print_exc()
+                a = 2
         except:
             pass
