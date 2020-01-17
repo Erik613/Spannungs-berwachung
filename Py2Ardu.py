cbaa59fcf94e1713b1de420206fd9ab31324c788
Patrick <patrickmatern1997@gmail.com>
2020-01-17 08:25:10 +0100

    Erweiterung des Schreibes in eine Datei

--- Py2Ardu.py
+++ Py2Ardu.py
@@ -9,15 +9,22 @@ while True:
         try:
             with serial.Serial('COM3', 9600, ) as ser:
                 x = ser.read()
-                b = datetime.datetime.now().strftime("%d.%m.%Y - %H:%M:%S") , "|" , int(x)
-                print(b)
-                log.write(str(b)+"\n")
+                if float(x) > 1:
+                    b = datetime.datetime.now().strftime("%d.%m.%Y - %H:%M:%S")
+                    print(b + " | " + str(float(x)))
+                    log.write(str(b) + " | " + str(float(x)) + "\n")
+                else:
+                    print("Messung beendet")
+                    log.write(str(b) + " | " + str(float(x)) + " Messung beendet""\n")
+                    break
 
                 a = 0
         except serial.serialutil.SerialException:
             a += 1
             if a == 1:
-                print(datetime.datetime.now().strftime("%d.%m.%Y - %H:%M:%S"), "| Hardware wurde Entfernd")
+                c = datetime.datetime.now().strftime("%d.%m.%Y - %H:%M:%S"), "| Hardware wurde Entfernd"
+                print(c)
+                log.write(str(c))
                 traceback.print_exc()
                 a = 2
         except:
