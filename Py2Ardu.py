6ed090341597ad579af2d9dce99f94c56ff6129e
Patrick <patrickmatern1997@gmail.com>
2020-01-24 09:12:34 +0100

    Fertig (alles)

--- Py2Ardu.py
+++ Py2Ardu.py
@@ -1,32 +1,64 @@
 import serial
-import traceback
 import datetime
-import struct
+import traceback
+import sys
 
 
-with open("log.csv", "a") as log:
-    time = datetime.datetime.now().strftime("%d.%m.%Y um %H:%M:%S")
-    print("Messung Startet am " + str(time) + "\n")
-    log.write("Messung Startet am " + str( time) + "\n")
-    while True:
+def user_input():
+    end_value = input("End Wert Angeben ")
+    try:
+        while True:
+            end_value = int(end_value)
+            measurement(end_value)
+    except ValueError:
+        print("Bitte eine Zahl eingeben")
+        user_input()
+    except Exception:
+        print(traceback.print_exc())
+        user_input()
+
+def get_value():
+    with serial.Serial('COM3', 9600, ) as ser:
+        x = ser.read(4)
+        bytes(x).decode('UTF-8')
+        return float(x)
+
+def measurement(end_value):
+    with open("log.csv", "a") as log:
+        time = datetime.datetime.now().strftime("%d.%m.%Y um %H:%M:%S")
+        print("Messung Startet am " + str(time) + "\n")
+        log.write("Messung Startet am " + str(time) + "\n")
         try:
-            with serial.Serial('COM3', 9600, ) as ser:
-                x = ser.read(4)
-                bytes(x).decode('UTF-8')
-                x = float(x)
-                if x > 8:
-                    b = datetime.datetime.now().strftime('"%H:%M:%S"')
-                    print(b + " | " + str(x))
-                    log.write(str(b) + " ; " + '"' +str(x)+ '"' + "\n")
-                else:
-                    print("Messung erfolgreich beendet am " + str(time) + "\n")
-                    log.write(str(b) + " ; " + '"' + str(x)+ '"' +"\n""Messung erfolgreich beendet am " + str(time) + "\n""\n")
-                    break
+            while (x:= get_value()) > end_value:
+                b = datetime.datetime.now().strftime('"%H:%M:%S:%f"')
+                print(b + " | " + str(x))
+                log.write(str(b) + " ; " + '"' + str(x) + '"' + "\n")
+            print("Messung erfolgreich beendet am " + str(time) + "\n")
+            log.write(str(b) + " ; " + '"' + str(x) + '"' + "\n""Messung erfolgreich beendet am " + str(
+                time) + "\n""\n")
+            sys.exit(1)
 
         except serial.serialutil.SerialException:
-                c = datetime.datetime.now().strftime("%d.%m.%Y - %H:%M:%S")
-                print(c + " | Hardware wurde Entfernd")
-                log.write(str(c) + " | Hardware wurde Entfernd""\n""Messung wird vorzeitig beendet""\n""\n")
-                break
+            c = datetime.datetime.now().strftime("%d.%m.%Y - %H:%M:%S")
+            print(c + " | Hardware wurde Entfernd")
+            print(traceback.print_exc())
+            log.write(str(c) + " | Hardware wurde Entfernd""\n""Messung wird vorzeitig beendet""\n""\n")
+            sys.exit(1)
+
+        except ValueError:
+            c = datetime.datetime.now().strftime("%d.%m.%Y - %H:%M:%S")
+            print(c + " | Hardware wurde Entfernd")
+            print(traceback.print_exc())
+            log.write(str(c) + " | Hardware wurde Entfernd""\n""Messung wird vorzeitig beendet""\n""\n")
+            sys.exit(1)
+
         except Exception:
-            pass
+            c = datetime.datetime.now().strftime("%d.%m.%Y - %H:%M:%S")
+            print(c + " | Ein unerwarteter Fehler ist aufgetretten. Messung wird abgebrochen")
+            print(traceback.print_exc())
+            log.write(str(
+                c) + " | Ein unerwarteter Fehler ist aufgetretten. Messung wird abgebrochen""\n""Messung wird vorzeitig beendet""\n""\n")
+            sys.exit(1)
+
+if __name__ == "__main__":
+    user_input()
