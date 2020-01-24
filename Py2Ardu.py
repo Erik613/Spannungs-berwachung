9c552bb37e6dd89c67cfd98e97055a449ae37344
Patrick <patrickmatern1997@gmail.com>
2020-01-24 09:07:35 +0100

    Fertig (etwa)

--- Py2Ardu.py
+++ Py2Ardu.py
@@ -1,31 +1,32 @@
 import serial
 import traceback
 import datetime
+import struct
 
-a = 0
 
-while True:
-    with open("log.txt", "a") as log:
+with open("log.csv", "a") as log:
+    time = datetime.datetime.now().strftime("%d.%m.%Y um %H:%M:%S")
+    print("Messung Startet am " + str(time) + "\n")
+    log.write("Messung Startet am " + str( time) + "\n")
+    while True:
         try:
             with serial.Serial('COM3', 9600, ) as ser:
-                x = ser.read()
-                if float(x) > 1:
-                    b = datetime.datetime.now().strftime("%d.%m.%Y - %H:%M:%S")
-                    print(b + " | " + str(float(x)))
-                    log.write(str(b) + " | " + str(float(x)) + "\n")
+                x = ser.read(4)
+                bytes(x).decode('UTF-8')
+                x = float(x)
+                if x > 8:
+                    b = datetime.datetime.now().strftime('"%H:%M:%S"')
+                    print(b + " | " + str(x))
+                    log.write(str(b) + " ; " + '"' +str(x)+ '"' + "\n")
                 else:
-                    print("Messung beendet")
-                    log.write(str(b) + " | " + str(float(x)) + " Messung beendet""\n")
+                    print("Messung erfolgreich beendet am " + str(time) + "\n")
+                    log.write(str(b) + " ; " + '"' + str(x)+ '"' +"\n""Messung erfolgreich beendet am " + str(time) + "\n""\n")
                     break
 
-                a = 0
         except serial.serialutil.SerialException:
-            a += 1
-            if a == 1:
-                c = datetime.datetime.now().strftime("%d.%m.%Y - %H:%M:%S"), "| Hardware wurde Entfernd"
-                print(c)
-                log.write(str(c))
-                traceback.print_exc()
-                a = 2
-        except:
+                c = datetime.datetime.now().strftime("%d.%m.%Y - %H:%M:%S")
+                print(c + " | Hardware wurde Entfernd")
+                log.write(str(c) + " | Hardware wurde Entfernd""\n""Messung wird vorzeitig beendet""\n""\n")
+                break
+        except Exception:
             pass
